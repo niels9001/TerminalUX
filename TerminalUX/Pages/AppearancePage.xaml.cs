7052b75c316e29c9bd9f998f602b3a6a6be1485a
Niels Laute <niels.laute@live.nl>
2025-11-15 14:02:33 +0100

    Updates

--- TerminalUX/Pages/AppearancePage.xaml.cs
+++ TerminalUX/Pages/AppearancePage.xaml.cs
@@ -8,9 +8,9 @@ namespace TerminalUX.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class InteractionPage : Page
+    public sealed partial class AppearancePage : Page
     {
-        public InteractionPage()
+        public AppearancePage()
         {
             InitializeComponent();
         }
